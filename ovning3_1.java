b4c77de361a8b39137c40f39c0ec21b8dc210c91
Mustafa Abdulrazzaq <abmu16@nti.umea>
2017-10-02 09:18:04 +0200

    skriver i d och b

---
+++ ovning3_1.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+/*
+Skriva i, d och b
+Mustafa abdulrazzaq
+10/2/2017
+*/
+ public class ovning3_1{
+ 	public static void main(String[] args) {
+
+ 		int i;
+ 		double d;
+ 		byte b; 
+
+ 		i=500;
+ 		d=500;
+ 		b=500;
+	}
+}
